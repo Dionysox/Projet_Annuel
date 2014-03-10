c787184df5d674c70ff39c501dd9213a76f8e2a2
Dionysox <romainbasset1@gmail.com>
2014-03-10 14:53:37 +0100

    blblblbl

---
+++ Accueil.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>L'Oranger</title>
+	<link rel="stylesheet" type="text/css" href="CSS1.css">
+	<link rel="stylesheet" type="text/css" href="Baniere.css">
+</head>
+<body>
+	<h1>L'Oranger</h1>
+		<div class="rouge">
+			<table>
+				<tr>
+				<td>
+				<a><img src="Drapal2.png" alt="Drapal" class="baniere"></a>
+				</td>
+				</tr>
+			</table>	
+			<table border=0>
+				<tr>
+				<td><span class="bouton_c" id="baniere"><a>Consulter</a></span></td>
+				<td><span class="bouton_u" id="baniere"><a>Upload</a></span></td>
+				<td><span class="bouton_r" id="baniere"><a>Recherche</a></span></td>
+				<td><span class="bouton_s" id="baniere"><a>Suppression</a></span></td>
+			</table>	
+		</div>
+</body>
+</html>
---
+++ Baniere.php
@@ -0,0 +1,16 @@
+		<div>
+			<table>
+				<tr>
+				<td>
+				<a><img src="Drapal2.png" alt="Drapal" class="banniere"></a>
+				</td>
+				</tr>
+			</table>	
+			<table border=0>
+				<tr>
+				<td><span class="bouton_c"><a>Consulter</a></span></td>
+				<td><span class="bouton_u"><a>Upload</a></span></td>
+				<td><span class="bouton_r"><a>Recherche</a></span></td>
+				<td><span class="bouton_s"><a>Suppression</a></span></td>
+			</table>	
+		</div>
